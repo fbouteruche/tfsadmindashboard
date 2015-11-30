1159d795b265e622d3b598015294ca86787633a9
Vinzz <vincent.tollu@gmail.com>
2015-11-30 15:02:02 +0100

    New Commands project (console is for the extracts, commands for changes to the configuration)

---
+++ tfsadmindashboard/TfsAdminDashboardCommands/Command/AddReaderUserCommand.cs
@@ -0,0 +1,22 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFSAdminDashboard.DataAccess;
+
+namespace TfsAdminDashboardCommands.Command
+{
+    public class AddReaderUserCommand : TFSAccessHelper
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public AddReaderUserCommand() { }
+
+        public void Execute(CommandLineOptions args)
+        {
+            var projectCollections = CatalogNodeBrowsingHelper.GetProjectCollections(configurationServer.CatalogNode);
+        }
+    }
+}
