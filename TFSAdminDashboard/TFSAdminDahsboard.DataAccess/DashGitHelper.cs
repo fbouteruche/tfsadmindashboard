e3efbdb610f85ca42c17248346c8cfdbd9aabf58
Vincent Tollu <vincent.tollu@orange.com>
2016-04-21 17:56:24 +0200

    Work in progress

---
+++ TFSAdminDashboard/TFSAdminDahsboard.DataAccess/DashGitHelper.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.TeamFoundation.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TFSAdminDashboard.DTO;
+using Microsoft.TeamFoundation.Git.Client;
+using Microsoft.TeamFoundation.SourceControl.WebApi;
+
+namespace TFSAdminDashboard.DataAccess
+{
+
+    public class DashGitHelper
+    {
+        public static List<VersionControlItem> FeedGitData(TfsTeamProjectCollection tpc, string projectName)
+        {
+            List<VersionControlItem> versionControlItemCollection = new List<VersionControlItem>();
+
+
+            var gitService = tpc.GetService<GitRepositoryService>();
+
+            GitRepository repository = gitService.QueryRepositories(projectName).FirstOrDefault();
+
+            return versionControlItemCollection;
+        }
+    }
+}
