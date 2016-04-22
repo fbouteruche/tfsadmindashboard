a6a4cdda372f85071b44a6f5492e4322e27d8c12
Vinzz <vincent.tollu@gmail.com>
2016-04-22 15:57:20 +0200

    Add LibGit2Sharp

--- TFSAdminDashboard/TFSAdminDahsboard.DataAccess/DashGitHelper.cs
+++ TFSAdminDashboard/TFSAdminDahsboard.DataAccess/DashGitHelper.cs
@@ -1,12 +1,10 @@
-﻿using Microsoft.TeamFoundation.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TFSAdminDashboard.DTO;
+﻿using LibGit2Sharp;
+using Microsoft.TeamFoundation.Client;
 using Microsoft.TeamFoundation.Git.Client;
 using Microsoft.TeamFoundation.SourceControl.WebApi;
+using System.Collections.Generic;
+using System.Linq;
+using TFSAdminDashboard.DTO;
 
 namespace TFSAdminDashboard.DataAccess
 {
@@ -21,6 +19,28 @@ namespace TFSAdminDashboard.DataAccess
             var gitService = tpc.GetService<GitRepositoryService>();
 
             GitRepository repository = gitService.QueryRepositories(projectName).FirstOrDefault();
+
+            using (var repo = new Repository(repository.RemoteUrl))
+            {
+                var RFC2822Format = "ddd dd MMM HH:mm:ss yyyy K";
+
+                foreach (Commit c in repo.Commits.Take(15))
+                {
+                    //Console.WriteLine(string.Format("commit {0}", c.Id));
+
+                    if (c.Parents.Count() > 1)
+                    {
+                        //Console.WriteLine("Merge: {0}",
+                            //string.Join(" ", c.Parents.Select(p => p.Id.Sha.Substring(0, 7)).ToArray()));
+                    }
+
+                    //Console.WriteLine(string.Format("Author: {0} <{1}>", c.Author.Name, c.Author.Email));
+                    //Console.WriteLine("Date:   {0}", c.Author.When.ToString(RFC2822Format, CultureInfo.InvariantCulture));
+                    //Console.WriteLine();
+                    //Console.WriteLine(c.Message);
+                    //Console.WriteLine();
+                }
+            }
 
             return versionControlItemCollection;
         }
---
+++ tfsadmindashboard/TfsAdminDashboardCommands/Properties/VersionAssemblyInfo.cs
@@ -0,0 +1,5 @@
+// This is a generated file.  Do not commit it to version control and do not modify it.
+using System.Reflection;
+[assembly: AssemblyVersion("1.0.36.0")]
+[assembly: AssemblyFileVersion("1.0.36.0")]
+[assembly: AssemblyInformationalVersion("1.0.36.0")]
