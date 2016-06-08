d4e64aa931a1a906dbf7242f4b1d4fa956d11aa5
Vinzz <vincent.tollu@gmail.com>
2016-06-08 10:36:21 +0200

    Add Team members JsonBO

---
+++ TFSAdminDashboard/TFSDataService/JsonBusinessObjects/TeamMember.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFSDataService.JsonBusinessObjects
+{
+
+    public class TeamMemberRootobject
+    {
+        public TeamMember[] value { get; set; }
+        public int count { get; set; }
+    }
+
+    public class TeamMember
+    {
+        public string id { get; set; }
+        public string displayName { get; set; }
+        public string uniqueName { get; set; }
+        public string url { get; set; }
+        public string imageUrl { get; set; }
+    }
+
+}
