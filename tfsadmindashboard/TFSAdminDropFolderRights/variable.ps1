48100717f25c9f616523531f149a46090ed32123
Vinzz <vincent.tollu@gmail.com>
2016-07-27 15:25:06 +0200

    remove old project

--- tfsadmindashboard/TFSAdminDropFolderRights/variable.ps1
+++
@@ -1,9 +0,0 @@
-[Environment]::SetEnvironmentVariable("TfsUrl", "http://tfsurl:8080/tfs", "User")
-[Environment]::SetEnvironmentVariable("TfsLoginName", (Read-Host -Prompt TfsLoginName), "User")
-[Environment]::SetEnvironmentVariable("TfsPassword", (Read-Host -Prompt TfsPassword), "User")
-[Environment]::SetEnvironmentVariable("TfsDropFolder", (Read-Host -Prompt TfsExtractPath), "User")
-
-
-
-
-
