a8a6084948de8b190aabe94232f14d2f7f949a30
Vinzz <vincent.tollu@gmail.com>
2016-08-05 14:54:10 +0200

    ack for init script

--- TFSAdminDashboard/TFSDataService/init.ps1
+++ TFSAdminDashboard/TFSDataService/init.ps1
@@ -1,3 +1,20 @@
-[Environment]::SetEnvironmentVariable("TfsUrl", (Read-Host -Prompt 'TfsUrl ("http://tfsurl:8080/tfs")'), "Machine")
-[Environment]::SetEnvironmentVariable("TfsLoginName", (Read-Host -Prompt TfsLoginName), "Machine")
-[Environment]::SetEnvironmentVariable("TfsPassword", (Read-Host -Prompt TfsPassword), "Machine")
+"This packages assumes that the following environment variables are declared:
+* TfsUrl (like 'http://tfsurl:8080/tfs')
+* TfsLoginName (With AD prefix if necessary)
+* TfsPassword (unencrypted, will have to fix that)
+
+Do you want to define these now?"
+
+$answer = Read-Host "yes or no"
+
+while("yes","no" -notcontains $answer)
+{
+	$answer = Read-Host "yes or no"
+}
+
+if($answer -notcontains "no")
+{
+    [Environment]::SetEnvironmentVariable("TfsUrl", (Read-Host -Prompt 'TfsUrl ("http://tfsurl:8080/tfs")'), "Machine")
+    [Environment]::SetEnvironmentVariable("TfsLoginName", (Read-Host -Prompt TfsLoginName), "Machine")
+    [Environment]::SetEnvironmentVariable("TfsPassword", (Read-Host -Prompt TfsPassword), "Machine")
+}
