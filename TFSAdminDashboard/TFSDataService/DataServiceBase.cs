e84c0786dfef4ef1ac7c5894be535408b7cf9388
Vinzz <vincent.tollu@gmail.com>
2016-08-05 14:14:54 +0200

    better exception

--- TFSAdminDashboard/TFSDataService/DataServiceBase.cs
+++ TFSAdminDashboard/TFSDataService/DataServiceBase.cs
@@ -21,7 +21,7 @@ namespace TFSDataService
         {
             if (Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.User) == null)
             {
-                throw new Exception(string.Format("No {0} Environment Variable found, please create it.", variable));
+                throw new Exception(string.Format("No {0} Environment Variable found, please create it. Environment varables required: TfsUrl (like http://mytfs:8080/tfs), TfsLoginName (with AD prefix if necessary), and TfsPassword", variable));
             }
         }
     }
