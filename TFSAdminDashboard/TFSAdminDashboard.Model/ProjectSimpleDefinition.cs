b201e73e3ab860b2fe082c5b5e35da68f4695b4e
Vinzz <vincent.tollu@gmail.com>
2016-09-22 11:10:44 +0200

    start of the simple project test

---
+++ TFSAdminDashboard/TFSAdminDashboard.Model/ProjectSimpleDefinition.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFSAdminDashboard.DTO
+{
+    public class ProjectSimpleDefinition
+    {
+        /// <summary>
+        /// Name of the project
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// TFS internal ID
+        /// </summary>
+        public string Id { get; set; }
+        /// <summary>
+        /// TFS State
+        /// </summary>
+        public string State { get; set; }
+        /// <summary>
+        /// Project Url
+        /// </summary>
+        public string Url { get; set; }
+        /// <summary>
+        /// Report requirement Overview 
+        /// </summary>
+        public string ReportsUrl { get; set; }
+
+        /// <summary>
+        /// Date of first git commit, or date of the platform's installation
+        /// </summary>
+        public DateTime? UtcCreationDate { get; set; }
+
+        /// <summary>
+        /// Did any commit occur in the last 10 days?
+        /// </summary>
+        public bool IsActive { get; set; }
+
+        /// <summary>
+        /// Last commit date
+        /// </summary>
+        public DateTime? LastCommit { get; set; }
+
+        /// <summary>
+        /// Commit number, grand total
+        /// </summary>
+        public int CommitsNumber { get; set; }
+
+        /// <summary>
+        /// Number of build definitions
+        /// </summary>
+        public int BuildNumber { get; set; }
+
+        /// <summary>
+        /// Percentage of succeed build over the last five ones
+        /// Mimics the Jenkins sun/cloud indicator => Average on all definitions
+        /// </summary>
+        public double BuildHealth { get; set; }
+
+        /// <summary>
+        /// Number of workitems
+        /// </summary>
+        public int WorkItemNumber { get; set; }
+
+        /// <summary>
+        /// % of closed workitems
+        /// </summary>
+        public double WorkItemHealth { get; set; }
+        
+        /// <summary>
+        /// Number of test cases
+        /// </summary>
+        public int TestNumber { get; set; }
+
+        /// <summary>
+        /// % of passed tests
+        /// </summary>
+        public double TestHealth { get; set; }
+    }
+}
---
+++ tfsadmindashboard/TfsAdminDashboardConsole/Commands/ExtractSimpleProjectListCommand.cs
@@ -0,0 +1,61 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using TFSAdminDashboard.DataAccess;
+using TFSAdminDashboard.DTO;
+using TfsAdminDashboardConsole.Commands.IO;
+using TfsAdminDashboardConsole.ExtensionsMethods;
+using TfsAdminDashboardConsole.Service;
+
+namespace TfsAdminDashboardConsole.Commands
+{
+    public class ExtractSimpleProjectListCommand : iCommand
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public ExtractSimpleProjectListCommand() { }
+
+        public void Execute(CommandLineOptions args)
+        {
+            try
+            {
+                logger.Info("Extract Simple Project List in progress...");
+                ICollection<ProjectSimpleDefinition> projectList = DashTeamProjectHelper.GetAllProjectsSimple();
+
+                string fileName = FileNameTool.GetFileName("TfsExtractProjectSimpleList", args.OutputFormat);
+
+                if (args.OutputFormat == "CSV")
+                {
+                    using (CsvWriter csv = new CsvWriter(new StreamWriter(fileName)))
+                    {
+                        csv.Configuration.RegisterClassMap<ProjectDefinitionCsvMap>();
+                        csv.WriteExcelSeparator();
+                        csv.WriteRecords(projectList);
+                    }
+                }
+                else
+                {
+                    string json = JsonConvert.SerializeObject(projectList);
+                    File.WriteAllText(fileName, json);
+                }
+
+                if (args.UploadSFTP)
+                {
+                    logger.Info("STFP Upload for file {1}", fileName);
+                    ISFTPService sftp = new SFTPService();
+                    sftp.UploadFile(fileName, args.SFTPAuthentMode.ToEnum<AuthentMethod>());
+                }
+
+                logger.Info("Extract Project done");
+            }
+            catch (Exception e)
+            {
+                logger.Error<Exception>(e);
+                throw;
+            }
+        }
+    }
+}
